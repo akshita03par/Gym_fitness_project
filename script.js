f9580eea4b89dd74816df7639a7aa7bc76bcb210
akshita03par <akshitaparmar2023@gmail.com>
2024-12-02 17:28:04 +0530

    some changes

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ menu.onscroll = () => {
     navbar.classList.remove('active');
 }
 
+// body max index 
+
+  function calculateBMI() {
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+
+    if (weight && height) {
+      const heightInMeters = height / 100;
+      const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+
+      document.getElementById('bmi-value').textContent = bmi;
+
+      let category = '';
+      if (bmi < 18.5) {
+        category = 'Underweight';
+      } else if (bmi >= 18.5 && bmi < 24.9) {
+        category = 'Normal weight';
+      } else if (bmi >= 25 && bmi < 29.9) {
+        category = 'Overweight';
+      } else {
+        category = 'Obesity';
+      }
+      document.getElementById('bmi-category').textContent = `Category: ${category}`;
+    } else {
+      alert('Please enter both weight and height!');
+    }
+  }
+
